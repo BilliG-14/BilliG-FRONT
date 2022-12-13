6dc4ee56f754c2769268476dd1e5e9c7a49e3614
Daekuen <daekuenhan@gmail.com>
2022-12-14 02:15:46 +0900

    fix: 초기 셋팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import Button from 'components/Button';
 import React from 'react';
 import './App.css';
 import logo from './logo.svg';
@@ -11,6 +12,7 @@ function App() {
         <h1 className="text-3xl font-bold underline text-red-600">
           Simple React Typescript Tailwind Sample!!!
         </h1>
+        <Button />
       </header>
     </div>
   );
---
+++ client/src/components/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Button() {
+  return (
+    <div>
+      <button>이거 눌러봐</button>
+    </div>
+  );
+}
