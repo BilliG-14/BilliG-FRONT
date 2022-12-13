7130201ee77606b64774531a877fc13751f9ef99
Daekuen <daekuenhan@gmail.com>
2022-12-13 23:45:45 +0900

    Setting: yanr1+CRA+TS, eslint, pritter, zustand, react-query, mui, tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>빌려줘14죠@@@!@!@!@@!</p>
+        <h1 className="text-3xl font-bold underline text-red-600">
+          Simple React Typescript Tailwind Sample!!!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
