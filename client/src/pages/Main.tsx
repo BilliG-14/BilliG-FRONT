c70bd84229820238fae6dc2cbf01a680420440ad
5thwin <hisy2059@naver.com>
2022-12-14 05:49:48 +0900

    feat Main first section

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,26 +1,51 @@
 import MainButton from 'components/MainButton';
-import React from 'react';
+import background from '../assets/images/first-main-image.png';
 
+function MainFirstSection() {
+  return (
+    <section
+      className="w-screen h-screen"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'contain',
+      }}
+    >
+      <div className="text-left p-32 pt-44 transition-all">
+        <p className="text-4xl">물건을 빌리는 가장 쉬운 방법</p>
+        <p className="text-7xl">초간편 물품 대여 중계 플랫폼</p>
+        <p className="text-7xl text-amber-500	">빌리지</p>
+      </div>
+    </section>
+  );
+}
+function MainSecondSection() {
+  return (
+    <section className="w-screen h-screen">
+      <p className="text-4xl my-24">
+        빌리지로 물품 대여 시간과 비용을 절약하세요! 최대 65% 절감 효과!
+      </p>
+      <div>
+        <div>
+          step 1. 빌려 주실 물품의 사진과 이름, 대여 비용과 상세 설명을
+          작성해주세요!
+        </div>
+        <div>
+          step 2. 물품을 빌려가실 때, 상대방에게 바로 대화를 걸어보세요!
+        </div>
+        <div>
+          step 3. 물품을 빌려가실 때, 대여기간을 설정한 후 대여 요청을
+          보내주세요!
+        </div>
+        <div>step 4. 대여하신 물품은 깨끗하게 사용하시고 반납해주세요</div>
+      </div>
+    </section>
+  );
+}
 export default function Main() {
   return (
     <>
-      <div>
-        <p className="text-2xl">물건을 빌리는 가장 쉬운 방법</p>
-        <h1>초간편 물품 대여 중계 플랫폼</h1>
-        <h1>빌리지</h1>
-      </div>
-      <div>
-        빌리지로 물품 대여 시간과 비용을 절약하세요! 최대 65% 절감 효과!
-      </div>
-      <div>
-        step 1. 빌려 주실 물품의 사진과 이름, 대여 비용과 상세 설명을
-        작성해주세요!
-      </div>
-      <div>step 2. 물품을 빌려가실 때, 상대방에게 바로 대화를 걸어보세요!</div>
-      <div>
-        step 3. 물품을 빌려가실 때, 대여기간을 설정한 후 대여 요청을 보내주세요!
-      </div>
-      <div>step 4. 대여하신 물품은 깨끗하게 사용하시고 반납해주세요</div>
+      <MainFirstSection />
+      <MainSecondSection />
       <MainButton content="물품 빌리러 가기!" />
       <MainButton content="물품 빌려주러 가기!" />
     </>
