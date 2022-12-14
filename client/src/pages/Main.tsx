e1dd228605b06f62bf583c6a665d5dd01747de3b
5thwin <hisy2059@naver.com>
2022-12-14 17:16:30 +0900

    feat Main SpeachBubble 추가

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -18,7 +18,9 @@ function MainSecondSection() {
   return (
     <section className="w-screen h-screen">
       <SpeechBubbleLeft content="너무 비싸서 사는 건 부담이에요" />
-      <SpeechBubbleRight content="너무 좋아요" />
+      <SpeechBubbleRight content="안쓰는 걸 빌려주고 돈을 벌어요!" />
+      <SpeechBubbleLeft content="환경오염을 줄이고 싶어요" />
+      <SpeechBubbleRight content="필요한 사람에게 대여해줄래요!" />
     </section>
   );
 }
