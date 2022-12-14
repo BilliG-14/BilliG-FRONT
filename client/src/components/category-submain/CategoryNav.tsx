22fdc3a0d831d66cbcf58ace991b3c7decafc740
Daekuen <daekuenhan@gmail.com>
2022-12-15 02:55:41 +0900

    Fix: category Nav-map함수  작성

--- client/src/components/category-submain/CategoryNav.tsx
+++ client/src/components/category-submain/CategoryNav.tsx
@@ -1,25 +1,26 @@
 export default function CategoryNav() {
+  const categoryList: { id: number; title: string }[] = [
+    { id: 1, title: 'IT기기' },
+    { id: 2, title: '생활가전' },
+    { id: 3, title: '캠핑/여행' },
+    { id: 4, title: '스포츠/레저' },
+    { id: 5, title: '완구/취미' },
+    { id: 6, title: '도서/음반' },
+  ];
+
   return (
     <nav className="flex bg-b-yellow w-100 h-16">
       <ul className="flex space-x-10 text-center items-center m-auto text-xl font-extrabold ">
-        <li className="text-b-hash-text hover:scale-125 ease-out duration-300">
-          <a href="#">IT기기</a>
-        </li>
-        <li className="text-b-hash-text hover:scale-125 ease-out duration-300">
-          <a href="#">생활가전</a>
-        </li>
-        <li className="text-b-hash-text hover:scale-125 ease-out duration-300">
-          <a href="#">캠핑/여행</a>
-        </li>
-        <li className="text-b-hash-text hover:scale-125 ease-out duration-300">
-          <a href="#">스포츠/레저</a>
-        </li>
-        <li className="text-b-hash-text hover:scale-125 ease-out duration-300">
-          <a href="#">완구/취미</a>
-        </li>
-        <li className="text-b-hash-text hover:scale-125 ease-out duration-300">
-          <a href="#">도서/음반</a>
-        </li>
+        {categoryList.map((category) => {
+          return (
+            <li
+              key={category.id}
+              className="text-b-hash-text hover:scale-125 ease-out duration-300"
+            >
+              <a href="#">{category.title}</a>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
