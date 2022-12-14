372112e75e51e4821e1965df4d763e1d09000fc4
Daekuen <daekuenhan@gmail.com>
2022-12-15 03:41:49 +0900

    Fix: category component 분리

--- client/src/components/category-submain/Category.tsx
+++ client/src/components/category-submain/Category.tsx
@@ -1,13 +1,65 @@
 import CategoryNav from './CategoryNav';
 import ItemCard from './ItemCard';
+import React, { useState } from 'react';
+
+export type ItemType = {
+  id: number;
+  img: string;
+  title: string;
+  category: string;
+  address: string;
+  deal: string;
+  price: { time: string; day: string };
+};
 
 export default function Category() {
+  const [itemList, setItemList] = useState<ItemType[]>([
+    {
+      id: 1,
+      img: 'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-midnight-select-20220606?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1653084303665',
+      title: 'Apple 2022 맥북 프로 13 M2 대여해드려요',
+      category: 'IT기기',
+      address: '서울시 동대문구',
+      deal: '직거래',
+      price: { time: '5,000', day: '30,000' },
+    },
+    {
+      id: 2,
+      img: 'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-midnight-select-20220606?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1653084303665',
+      title: 'Apple 2022 맥북 프로 13 M2 대여해드려요',
+      category: 'IT기기',
+      address: '서울시 동대문구',
+      deal: '택배',
+      price: { time: '5,000', day: '30,000' },
+    },
+    {
+      id: 3,
+      img: 'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-midnight-select-20220606?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1653084303665',
+      title: 'Apple 2022 맥북 프로 13 M2 대여해드려요',
+      category: 'IT기기',
+      address: '서울시 동대문구',
+      deal: '직거래',
+      price: { time: '5,000', day: '30,000' },
+    },
+    {
+      id: 4,
+      img: 'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-midnight-select-20220606?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1653084303665',
+      title: 'Apple 2022 맥북 프로 13 M2 대여해드려요',
+      category: 'IT기기',
+      address: '서울시 동대문구',
+      deal: '택배',
+      price: { time: '5,000', day: '30,000' },
+    },
+  ]);
+
   return (
     <div className="">
       <CategoryNav />
-      <div className="max-w-screen-lg ">
-        <ItemCard />
-      </div>
+      <section className="max-w-screen-lg flex content-center items-center">
+        {itemList.map((item) => (
+          <ItemCard key={item.id} item={item} />
+        ))}
+      </section>
     </div>
   );
 }
--- client/src/components/category-submain/ItemCard.tsx
+++ client/src/components/category-submain/ItemCard.tsx
@@ -1,36 +1,39 @@
-import { FaPeopleArrows } from 'react-icons/fa';
+import DealTag from 'components/tag/DealTag';
+import { ItemType } from './Category';
 
-export default function ItemCard() {
+type ItemProps = {
+  item: ItemType;
+};
+
+export default function ItemCard({ item }: ItemProps) {
+  const { img, title, category, address, deal, price } = item;
+
   return (
-    <div className="w-1/5 inline-block my-5 px-2.5 border-b border-gray-300 border-solid ">
+    <div className="w-1/5 inline-block my-5 px-2.5 border-b border-gray-300 border-solid mx-4">
       <a href="#">
         <div className="pic mb-2">
-          <img
-            src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-midnight-select-20220606?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1653084303665"
-            alt="m2 맥북"
-          />
+          <img src={img} alt="m2 맥북" />
         </div>
         <div className="item_info my-2 text-left">
           <div className="title ">
             <p className="name mb-1 font-bold leading-6 underline underline-offset-4">
-              Apple 2022 맥북 프로 13 M2 대여해드려요
+              {title}
             </p>
-            <p className="category mb-1 text-b-text-darkgray text-sm">IT기기</p>
+            <p className="category mb-1 text-b-text-darkgray text-sm">
+              {category}
+            </p>
             <p className="adress text-b-text-darkgray text-sm">
-              📍서울시 동대문구
+              {`📍 ${address}`}
             </p>
           </div>
-          <div className="item_tag inline-flex bg-b-tag-dir text-b-hash-text p-1 rounded-lg font-extrabold my-2">
-            <FaPeopleArrows className="mr-1 text-sm" />
-            <span className="text-xs">직거래</span>
-          </div>
+          <DealTag deal={deal} />
           <div className="price text-right">
             <p className="per_time mb-2">
-              <span className="font-semibold"> 5,000원</span>
+              <span className="font-semibold"> {`${price.time}원`}</span>
               <span className="text-xs"> / 시간</span>
             </p>
             <p className="per_day">
-              <span className="font-semibold"> 30,000원</span>
+              <span className="font-semibold"> {`${price.day}원`}</span>
               <span className="text-xs"> / 일</span>
             </p>
           </div>
