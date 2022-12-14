7cf71a192f66bbafe7b8345910d100791b32f50a
5thwin <hisy2059@naver.com>
2022-12-14 15:13:10 +0900

    Feat Scroll Fade In custom hook

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,7 +1,6 @@
 import MainButton from 'components/MainButton';
 import ScrollTopButton from 'components/ScrollTopButton';
-import { useEffect, useState } from 'react';
-import background from '../assets/images/first-main-image.png';
+import useScrollFadeIn from '../hooks/useScrollFadeIn';
 
 function MainFirstSection() {
   return (
@@ -16,20 +15,9 @@ function MainFirstSection() {
 }
 
 function MainSecondSection() {
-  const [position, setPosition] = useState(0);
-  function onScroll() {
-    console.log(window.scrollY);
-    setPosition(window.scrollY);
-  }
-  useEffect(() => {
-    window.addEventListener('scroll', onScroll);
-    return () => {
-      window.removeEventListener('scroll', onScroll);
-    };
-  }, []);
   return (
     <section className="w-screen h-screen">
-      <p className="text-4xl my-24" style={{ opacity: (position - 300) / 50 }}>
+      <p className="text-4xl my-24">
         <span className=" text-amber-500 font-extrabold">빌리지</span>로 물품
         대여 시간과 비용을 절약하세요!{' '}
         <p className="text-5xl">최대 65% 절감 효과!</p>
@@ -39,23 +27,19 @@ function MainSecondSection() {
           num={1}
           content="빌려 주실
           물품의 사진과 이름, 대여 비용과 상세 설명을 작성해주세요!"
-          position={position}
         />
         <StepDiscription
           num={2}
           content="물품을 빌려가실 때, 상대방에게 바로 대화를 걸어보세요!"
-          position={position}
         />
         <StepDiscription
           num={3}
           content="물품을 빌려가실 때, 대여기간을 설정한 후 대여 요청을
           보내주세요!"
-          position={position}
         />
         <StepDiscription
           num={4}
           content="대여하신 물품은 깨끗하게 사용하시고 반납해주세요"
-          position={position}
         />
       </div>
     </section>
@@ -74,15 +58,17 @@ export default function Main() {
     </>
   );
 }
+
 type StepProps = {
   num: number;
   content: string;
-  position: number;
 };
+
 function StepDiscription(props: StepProps) {
+  const fadeInDiv = useScrollFadeIn();
   return (
-    <div className="text-2xl" style={{ opacity: (props.position - 600) / 50 }}>
-      <span className="text-amber-600 font-bold">step {props.num}.</span>{' '}
+    <div className="text-2xl" {...fadeInDiv}>
+      <span className="text-amber-600 font-bold">{props.num}.</span>{' '}
       {props.content}
       <StepImageDiv />
     </div>
