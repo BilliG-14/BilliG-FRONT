7ce6f7a5790866aec6b3870e871fb59165d01954
5thwin <hisy2059@naver.com>
2022-12-16 05:27:54 +0900

    feat admin button transition

--- client/src/pages/AdminMain.tsx
+++ client/src/pages/AdminMain.tsx
@@ -1,19 +1,40 @@
-function ButtonDiv() {
+function AdminHeader() {
   return (
-    <div className="w-1/3 h-full">
-      <button className="bg-b-yellow mx-auto h-5/6 w-5/6"></button>
-    </div>
+    <>
+      {' '}
+      <p className="max-w-screen-lg mx-auto p-6 text-3xl font-extrabold">
+        Admin Page
+      </p>
+      <hr className="border-2 border-b-text-black max-w-screen-lg mx-auto"></hr>
+    </>
   );
 }
-
 export default function AdminMain() {
   return (
-    <div className="max-w-screen-lg bg-slate-200 mx-auto">
-      <div className="my-48 h-96 flex bg-green-200">
-        <ButtonDiv></ButtonDiv>
-        <ButtonDiv></ButtonDiv>
-        <ButtonDiv></ButtonDiv>
+    <>
+      <AdminHeader />
+      <div className="max-w-screen-lg mx-auto mt-36">
+        <div className="flex w-full">
+          <button className=" bg-b-bg-gray h-96 w-full mx-9 rounded-2xl group hover:bg-b-yellow  hover:text-white transition-colors shadow-lg">
+            <i className="fa-solid fa-user text-9xl group-hover:scale-110 transition-transform duration-700"></i>
+            <p className="text-2xl mt-6 font-semibold group-hover:font-extrabold">
+              회원 관리
+            </p>
+          </button>
+          <button className=" bg-b-bg-gray h-96 w-full mx-9 rounded-2xl group hover:bg-b-yellow  hover:text-white transition-colors shadow-lg">
+            <i className="fa-solid fa-file-pen text-9xl group-hover:scale-110 transition-transform duration-700"></i>
+            <p className="text-2xl mt-6 font-semibold group-hover:font-extrabold">
+              게시글 관리
+            </p>
+          </button>
+          <button className=" bg-b-bg-gray h-96 w-full mx-9 rounded-2xl group hover:bg-b-yellow  hover:text-white transition-colors shadow-lg">
+            <i className="fa-solid fa-handshake-angle text-9xl group-hover:scale-110 transition-transform duration-700"></i>
+            <p className="text-2xl mt-6 font-semibold group-hover:font-extrabold">
+              대여 관리
+            </p>
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
