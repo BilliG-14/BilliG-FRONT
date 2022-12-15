5461cc20975369e25b9e7ef3b68225eb5a585bc9
Gyuri092 <ggr0927@gmail.com>
2022-12-16 02:06:01 +0900

    feat: nav바 ui 완성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from 'pages/Main';
 import LoginJoin from './pages/LoginJoin';
 import Submain from './pages/Submain';
-import Nav from '../src/components/nav/Nav';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
@@ -19,10 +18,13 @@ function App() {
       <div className="App h-screen">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/login" element={<LoginJoin />} />
-            <Nav />
-            <Submain />
+            {/* 아래 부분은 승연님이 작성하신 feat-FE-join의 부분이고,
+             이걸 그대로 두면 충돌이 나므로 주석처리했습니다.
+             원래는 라우팅을 제대로 손봐야하는데, 제 브랜치에서 이걸 건드리는것은
+             뭔가 아닌것 같아서 ^^; 일단 주석처리하고 nav먼저 만들어놨습니다. */}
+            {/* <Route path="/" element={<Main />} />
+            <Route path="/login" element={<LoginJoin />} /> */}
+            <Route path="/" element={<Submain />} />
           </Routes>
         </BrowserRouter>
       </div>
--- client/src/components/nav/Nav.tsx
+++ client/src/components/nav/Nav.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FiSearch } from 'react-icons/fi';
 
 function Nav() {
   const navigate = useNavigate();
+  const goHome = () => {
+    navigate('/');
+  };
   const goLogin = () => {
     navigate('/login');
   };
@@ -12,16 +16,51 @@ function Nav() {
   const goMyPage = () => {
     navigate('/mypage');
   };
+  const goSearch = () => {
+    navigate('/search');
+  };
   return (
-    <div>
+    <div className="flex justify-between pr-5 h-32">
       <div>
-        <h1 className="font-extrabold text-5xl mt-9">B illi G</h1>
-        {/* 여기에 빌리지 로고를 넣으면 됩니다! */}
+        <button
+          className="home  hover:text-b-yellow hover: ease-in-out duration-300"
+          onClick={goHome}
+        >
+          <h1 className="font-extrabold text-5xl mt-9">B illi G</h1>
+        </button>
+        {/* 위의 button과 h1태그를 지우고 여기에 빌리지 로고를 넣으면 됩니다! */}
       </div>
-      <div>
-        <button className="login" onClick={goLogin}></button>
-        <button className="join" onClick={goJoin}></button>
-        <button className="mypage" onClick={goMyPage}></button>
+      <div className="flex flex-col justify-center">
+        <div className="flex justify-between items-center w-52 text-lg font-semibold">
+          <button
+            className="login hover:text-b-yellow hover: ease-in-out duration-300"
+            onClick={goLogin}
+          >
+            login
+          </button>
+          /
+          <button
+            className="join hover:text-b-yellow hover: ease-in-out duration-300"
+            onClick={goJoin}
+          >
+            join
+          </button>
+          /
+          <button
+            className="mypage hover:text-b-yellow hover: ease-in-out duration-300"
+            onClick={goMyPage}
+          >
+            myPage
+          </button>
+        </div>
+        <div className="flex justify-end text-3xl mt-5">
+          <button
+            className="search hover:text-b-yellow hover: ease-in-out duration-300"
+            onClick={goSearch}
+          >
+            <FiSearch />
+          </button>
+        </div>
       </div>
     </div>
   );
