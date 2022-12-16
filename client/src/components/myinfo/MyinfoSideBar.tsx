e414609c722a4b654912b52690838e83cd215e64
Daekuen <daekuenhan@gmail.com>
2022-12-16 23:44:43 +0900

    Fix : myInfo sidebar layout

--- client/src/components/myinfo/MyinfoSideBar.tsx
+++ client/src/components/myinfo/MyinfoSideBar.tsx
@@ -3,41 +3,55 @@ export default function MyInfoSideBar() {
     <div className="w-1/5 h-full p-4 bg-b-bg-gray">
       <div className="menu_box mb-7">
         <p className="items-center justify-center text-xl font-extrabold mb-1">
-          회원 관리
+          냐 정보
         </p>
         <ul className="text-center text-lg font-semibold">
           <li className="h-10 flex items-center justify-start">
             <a href="#!">
-              <p className="items-center justify-center">회원 목록 조회</p>
-            </a>
-          </li>
-          <li className="h-10 flex items-center justify-start">
-            <a href="#!">
-              <p className="items-center justify-center">회원 상세 관리</p>
+              <p className="items-center justify-center">개인정보확인/수정</p>
             </a>
           </li>
         </ul>
       </div>
       <div className="menu_box mb-7">
         <p className="items-center justify-center text-xl font-extrabold mb-1">
-          게시물 관리
+          내 활동
         </p>
         <ul className="text-center text-lg font-semibold">
           <li className="h-10 flex items-center justify-start">
             <a href="#!">
-              <p className="items-center justify-center">게시물 조회</p>
+              <p className="items-center justify-center">
+                빌려주기 게시물 조회
+              </p>
+            </a>
+          </li>
+          <li className="h-10 flex items-center justify-start">
+            <a href="#!">
+              <p className="items-center justify-center">빌리기 게시물 조회</p>
             </a>
           </li>
         </ul>
       </div>
       <div className="menu_box mb-5">
         <p className="items-center justify-center text-xl font-extrabold mb-1">
-          대여 관리
+          내 거래
         </p>
         <ul className="text-center text-lg font-semibold">
           <li className="h-10 flex items-center justify-start">
             <a href="#!">
-              <p className="items-center justify-center">대여 현황 조회</p>
+              <p className="items-center justify-center">
+                내가 빌려준 물품 조회
+              </p>
+            </a>
+          </li>
+          <li className="h-10 flex items-center justify-start">
+            <a href="#!">
+              <p className="items-center justify-center">내가 빌린 물품 조회</p>
+            </a>
+          </li>
+          <li className="h-10 flex items-center justify-start">
+            <a href="#!">
+              <p className="items-center justify-center">수익금 조회</p>
             </a>
           </li>
         </ul>
