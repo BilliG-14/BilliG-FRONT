9143b8c82a8afc7485878f8df09da2dc35fa7373
5thwin <hisy2059@naver.com>
2022-12-16 14:27:52 +0900

    Feat admin header 분리

---
+++ client/src/components/admin/AdminHeader.tsx
@@ -0,0 +1,11 @@
+export default function AdminHeader() {
+  return (
+    <>
+      {' '}
+      <p className="max-w-screen-lg mx-auto p-6 text-3xl font-extrabold">
+        Admin Page
+      </p>
+      <hr className="border-2 border-b-text-black max-w-screen-lg mx-auto"></hr>
+    </>
+  );
+}
--- client/src/pages/AdminMain.tsx
+++ client/src/pages/AdminMain.tsx
@@ -1,14 +1,5 @@
-function AdminHeader() {
-  return (
-    <>
-      {' '}
-      <p className="max-w-screen-lg mx-auto p-6 text-3xl font-extrabold">
-        Admin Page
-      </p>
-      <hr className="border-2 border-b-text-black max-w-screen-lg mx-auto"></hr>
-    </>
-  );
-}
+import AdminHeader from 'components/admin/AdminHeader';
+
 export default function AdminMain() {
   return (
     <>
