fb9645de689b666d1f4cf208adc60e880f763591
5thwin <hisy2059@naver.com>
2022-12-16 13:35:20 +0900

    feat 디렉토리 정리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from 'pages/Main';
 import LoginJoin from './pages/LoginJoin';
