c7fb83e548db4f9efb33b71e03b9a01fa3aae6ec
5thwin <hisy2059@naver.com>
2022-12-16 20:02:15 +0900

    feat navbar

--- client/src/components/admin/AdminHeader.tsx
+++ client/src/components/admin/AdminHeader.tsx
@@ -2,9 +2,11 @@ export default function AdminHeader() {
   return (
     <>
       {' '}
-      <p className="max-w-screen-lg mx-auto p-6 text-3xl font-extrabold">
-        Admin Page
-      </p>
+      <a href="/admin">
+        <p className="max-w-screen-lg mx-auto p-6 text-3xl font-extrabold">
+          Admin Page
+        </p>
+      </a>
       <hr className="border-2 border-b-text-black max-w-screen-lg mx-auto"></hr>
     </>
   );
--- client/src/pages/AdminMain.tsx
+++ client/src/pages/AdminMain.tsx
@@ -1,11 +1,37 @@
 import AdminHeader from 'components/admin/AdminHeader';
 import AdminMainSection from 'components/admin/AdminMainSection';
+import create from 'zustand/react';
 
+// interface adminPageState {
+
+// }
+// const useAdminStore = create<adminPageState>((set) => { });
+function AdminManageUserSection() {
+  return (
+    <section className="w-full h-96">
+      <div className="h-10 border-b-4 border-b-text-black border-solid text-xl font-semibold text-b-text-black py-1">
+        <i className="fa-solid fa-user p-1" />
+        <span className="">회원 관리</span>
+      </div>
+      <div className="flex">
+        <ul className="w-1/5 h-full border-r-4 border-b-text-black border- text-center text-lg font-extrabold">
+          <li className="h-8 border-b-4 border-b-text-black border-solid">
+            <a href="#!">
+              <p className="w-full h-8 p-3">item 1</p>
+            </a>
+          </li>
+        </ul>
+        <div>메인영역</div>
+      </div>
+    </section>
+  );
+}
 export default function AdminMain() {
   return (
-    <>
+    <div className="w-screen max-w-screen-lg">
       <AdminHeader />
-      <AdminMainSection />
-    </>
+      {/* <AdminMainSection /> */}
+      <AdminManageUserSection />
+    </div>
   );
 }
