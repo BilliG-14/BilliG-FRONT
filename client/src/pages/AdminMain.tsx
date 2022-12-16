8248def0e932231e4989662d168318c591989e17
5thwin <hisy2059@naver.com>
2022-12-16 14:41:26 +0900

    feat admin button section 분리

--- client/src/pages/AdminMain.tsx
+++ client/src/pages/AdminMain.tsx
@@ -1,31 +1,11 @@
 import AdminHeader from 'components/admin/AdminHeader';
+import AdminMainSection from 'components/admin/AdminMainSection';
 
 export default function AdminMain() {
   return (
     <>
       <AdminHeader />
-      <div className="w-screen max-w-screen-lg mx-auto mt-36">
-        <div className="flex w-full">
-          <button className=" bg-b-bg-gray h-96 w-full mx-9 rounded-2xl group hover:bg-b-yellow  hover:text-white transition-colors shadow-lg">
-            <i className="fa-solid fa-user text-9xl group-hover:scale-110 transition-transform duration-700"></i>
-            <p className="text-2xl mt-6 font-semibold group-hover:font-extrabold">
-              회원 관리
-            </p>
-          </button>
-          <button className=" bg-b-bg-gray h-96 w-full mx-9 rounded-2xl group hover:bg-b-yellow  hover:text-white transition-colors shadow-lg">
-            <i className="fa-solid fa-file-pen text-9xl group-hover:scale-110 transition-transform duration-700"></i>
-            <p className="text-2xl mt-6 font-semibold group-hover:font-extrabold">
-              게시글 관리
-            </p>
-          </button>
-          <button className=" bg-b-bg-gray h-96 w-full mx-9 rounded-2xl group hover:bg-b-yellow  hover:text-white transition-colors shadow-lg">
-            <i className="fa-solid fa-handshake-angle text-9xl group-hover:scale-110 transition-transform duration-700"></i>
-            <p className="text-2xl mt-6 font-semibold group-hover:font-extrabold">
-              대여 관리
-            </p>
-          </button>
-        </div>
-      </div>
+      <AdminMainSection />
     </>
   );
 }
