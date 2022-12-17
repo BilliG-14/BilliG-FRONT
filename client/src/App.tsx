1f2db9dce060694a2bc08fd0a0eccfcdc65463d2
Daekuen <daekuenhan@gmail.com>
2022-12-17 10:04:41 +0900

    fix : conflict 코드 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,13 +8,10 @@ import reset from 'styled-reset';
 import BorrowWriting from 'pages/BorrowWriting';
 import GiveWriting from 'pages/GiveWriting';
 import Submain from './pages/Submain';
-<<<<<<< HEAD
 import Search from './pages/Search';
 import MyInfo from './pages/MyInfo';
-=======
 import GivePostDetail from './pages/GivePostDetail';
 import BorrowPostDetail from './pages/BorrowPostDetail';
->>>>>>> 488975395b5aae2d6587d2eba812ceaa17ba4092
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
@@ -32,14 +29,11 @@ function App() {
             <Route path="/admin" element={<AdminMain />} />
             <Route path="/write/give" element={<GiveWriting />} />
             <Route path="/write/borrow" element={<BorrowWriting />} />
-<<<<<<< HEAD
             <Route path="/submain" element={<Submain />} />
             <Route path="/search" element={<Search />} />
             <Route path="/myinfo" element={<MyInfo />} />
-=======
             <Route path="/read/give" element={<GivePostDetail />} />
             <Route path="/read/borrow" element={<BorrowPostDetail />} />
->>>>>>> 488975395b5aae2d6587d2eba812ceaa17ba4092
           </Routes>
         </BrowserRouter>
       </div>
