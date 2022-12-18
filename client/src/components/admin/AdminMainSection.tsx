00ae48749f3f50a8af1fa4de091e7feed776a68f
5thwin <hisy2059@naver.com>
2022-12-18 22:54:51 +0900

    feat 관리자 mainsection

--- client/src/components/admin/AdminMainSection.tsx
+++ client/src/components/admin/AdminMainSection.tsx
@@ -1,26 +1,31 @@
+import {
+  AdminUserListSection,
+  AdminUserDetailSection,
+  AdminReportSection,
+  AdminNoticeSection,
+} from 'components/admin';
+import useAdminPageStore, { AdminSection } from 'store/AdminPageStore';
+
 export default function AdminMainSection() {
-  return (
-    <section className="w-screen max-w-screen-lg mx-auto mt-36">
-      <div className="flex w-full">
-        <button className=" bg-b-bg-gray h-96 w-full mx-9 rounded-2xl group hover:bg-b-yellow  hover:text-white transition-colors shadow-lg">
-          <i className="fa-solid fa-user text-9xl group-hover:scale-110 transition-transform duration-700"></i>
-          <p className="text-2xl mt-6 font-semibold group-hover:font-extrabold">
-            회원 관리
-          </p>
-        </button>
-        <button className=" bg-b-bg-gray h-96 w-full mx-9 rounded-2xl group hover:bg-b-yellow  hover:text-white transition-colors shadow-lg">
-          <i className="fa-solid fa-file-pen text-9xl group-hover:scale-110 transition-transform duration-700"></i>
-          <p className="text-2xl mt-6 font-semibold group-hover:font-extrabold">
-            게시글 관리
-          </p>
-        </button>
-        <button className=" bg-b-bg-gray h-96 w-full mx-9 rounded-2xl group hover:bg-b-yellow  hover:text-white transition-colors shadow-lg">
-          <i className="fa-solid fa-handshake-angle text-9xl group-hover:scale-110 transition-transform duration-700"></i>
-          <p className="text-2xl mt-6 font-semibold group-hover:font-extrabold">
-            대여 관리
-          </p>
-        </button>
-      </div>
-    </section>
-  );
+  const section = useAdminPageStore((state) => state.section);
+  const showSection = ((_section: AdminSection) => {
+    switch (_section) {
+      case AdminSection.USERLIST:
+        return <AdminUserListSection />;
+      case AdminSection.REPORT:
+        return <AdminReportSection />;
+      case AdminSection.USERDETAIL:
+        return <AdminUserDetailSection />;
+      case AdminSection.NOTICE:
+        return <AdminNoticeSection />;
+      case AdminSection.POST:
+        return;
+      case AdminSection.RENTAL:
+        return;
+      default:
+        return <section></section>;
+    }
+  })(section);
+
+  return <section className="w-full">{showSection}</section>;
 }
--- client/src/pages/AdminMain.tsx
+++ client/src/pages/AdminMain.tsx
@@ -1,45 +1,14 @@
-import {
-  AdminHeader,
-  AdminSideBar,
-  AdminUserListSection,
-  AdminUserDetailSection,
-  AdminReportSection,
-  AdminNoticeSection,
-} from 'components/admin';
-import useAdminPageStore, { AdminSection } from 'store/AdminPageStore';
-// interface adminPageState {
-
-// }
-// const useAdminStore = create<adminPageState>((set) => { });
+import { AdminHeader, AdminSideBar } from 'components/admin';
+import AdminMainSection from 'components/admin/AdminMainSection';
 
 export default function AdminMain() {
-  const section = useAdminPageStore((state) => state.section);
-  const showSection = ((_section: AdminSection) => {
-    switch (_section) {
-      case AdminSection.USERLIST:
-        return <AdminUserListSection />;
-      case AdminSection.REPORT:
-        return <AdminReportSection />;
-      case AdminSection.USERDETAIL:
-        return <AdminUserDetailSection />;
-      case AdminSection.NOTICE:
-        return <AdminNoticeSection />;
-      case AdminSection.POST:
-        return;
-      case AdminSection.RENTAL:
-        return;
-      default:
-        return <section></section>;
-    }
-  })(section);
-
   return (
     <div className="h-full w-screen max-w-screen-lg m-auto">
       <AdminHeader />
       <section className="max-w-screen-lg h-full">
         <div className="flex h-full">
           <AdminSideBar />
-          {showSection}
+          <AdminMainSection />
         </div>
       </section>
     </div>
