b30b5434afe5f7e7e2920785fa7e4c06a04a62f1
FROM-minyoung <918liter@gmail.com>
2022-12-20 21:44:24 +0900

    Feat: 업로드한 이미지 삭제 구현

--- client/src/components/postWrite/ImageUpload.tsx
+++ client/src/components/postWrite/ImageUpload.tsx
@@ -4,7 +4,7 @@ import { AiFillCamera } from 'react-icons/ai';
 
 export default function ImageUpload() {
   const { imgFiles, setImgFile } = imageUploadStore();
-  const [fileImage, setFileImage] = useState<string[]>([]);
+  const [priviewImages, setPriviewFileImages] = useState<string[]>([]);
 
   // 이미지 갯수 제한(가장 우선), 이미지 크기 제한 구현해야 함
   // 게시글을 볼 때 메인 이미지를 어떻게 정할것인지?
@@ -20,15 +20,25 @@ export default function ImageUpload() {
     }
 
     for (let i = 0; i < files.length; i++) {
-      setFileImage((cur) => [...cur, URL.createObjectURL(files[i])]);
+      setPriviewFileImages((cur) => [...cur, URL.createObjectURL(files[i])]);
     }
 
     setImgFile(files);
   }
 
   // 미리보기 이미지 클릭 시 해당 이미지 삭제
-  // function deleteImagehandler(e: React.MouseEvent<HTMLImageElement>) {}
+  function deleteImagehandler(e: React.MouseEvent<HTMLImageElement>) {
+    const previewId = Number(e.currentTarget.id);
+    const newFiles = imgFiles.filter((imgfile, idx) => idx !== previewId);
+    const newPreviewFiles = priviewImages.filter(
+      (imgfile, idx) => idx !== previewId,
+    );
 
+    setImgFile(newFiles);
+    setPriviewFileImages(newPreviewFiles);
+  }
+  console.log(imgFiles);
+  console.log(priviewImages);
   return (
     <section className="mb-4 flex flex-row">
       <label
@@ -36,7 +46,7 @@ export default function ImageUpload() {
         className="inline-block w-[70px] h-[70px] mr-3 text-center text-xs border-solid border border-gray-300 hover:border-gray-400 cursor-pointer rounded-lg"
       >
         <AiFillCamera className="w-6 h-6 mx-auto mt-3.5 mb-[2px]" />
-        <div className="text-gray-400">{fileImage.length} / 3</div>
+        <div className="text-gray-400">{priviewImages.length} / 3</div>
       </label>
       <input
         onChange={imagePreview}
@@ -48,14 +58,15 @@ export default function ImageUpload() {
         // className="block w-[500px] text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold cursor-pointer file:bg-b-bg-gray file:text-b-text-black  hover:file:bg-gray-200 file:cursor-pointer"
       />
       <div className="flex flex-row">
-        {fileImage.map((fileUrl) => {
+        {priviewImages.map((fileUrl, idx) => {
           return (
             <img
-              // onClick={deleteImagehandler}
+              onClick={deleteImagehandler}
+              id={idx.toString()}
               key={fileUrl}
               alt="이미지"
               src={fileUrl}
-              className="w-[70px] h-[70px] mr-2 border-solid border border-gray-300"
+              className="w-[70px] h-[70px] mr-2 border-solid border border-gray-300 rounded-lg"
             />
           );
         })}
