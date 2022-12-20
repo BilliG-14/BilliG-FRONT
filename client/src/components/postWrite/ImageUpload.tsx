ef68f46c232b9a0affda51ea7d983fbf3c42a4fb
FROM-minyoung <918liter@gmail.com>
2022-12-20 23:29:45 +0900

    Feat: 업로드한 이미지 삭제 구현(프리뷰 이미지 배열 형식)

--- client/src/components/postWrite/ImageUpload.tsx
+++ client/src/components/postWrite/ImageUpload.tsx
@@ -13,7 +13,9 @@ export default function ImageUpload() {
     // 유사배열 객체를 Array로 변환
     const files = Array.from(target.files as FileList);
 
-    if (imgFiles.length > 3) {
+    setPriviewFileImages([]);
+
+    if (files.length > 3) {
       alert('이미지는 최대 3개까지만 등록 가능합니다. ');
       return;
     } else {
@@ -43,7 +45,7 @@ export default function ImageUpload() {
     <section className="mb-4 flex flex-row ">
       <label
         htmlFor="fileUpload"
-        className="inline-block w-[70px] h-[70px] mr-3 text-center text-xs border-solid border border-gray-300 hover:border-gray-400 cursor-pointer rounded-lg"
+        className="inline-block w-[70px] h-[70px] mr-5 text-center text-xs border-solid border border-gray-300 hover:border-gray-400 cursor-pointer rounded-lg"
       >
         <AiFillCamera className="w-6 h-6 mx-auto mt-3.5 mb-[2px]" />
         <div className="text-gray-400">{priviewImages.length} / 3</div>
@@ -66,14 +68,14 @@ export default function ImageUpload() {
                 key={fileUrl}
                 alt="이미지"
                 src={fileUrl}
-                className="w-[70px] h-[70px] mr-2 border-solid border border-gray-300 rounded-lg"
+                className="w-[70px] h-[70px] border-solid border border-gray-300 rounded-lg"
               />
               <div
-                key={idx}
+                key={idx + 1}
                 onClick={deleteImagehandler}
-                className="relative w-5 h-5 bg-b-text-black rounded-full right-6 text-white text-center text-sm cursor-pointer"
+                className="relative w-5 h-5 bg-b-text-black rounded-full right-3 text-white text-center text-sm cursor-pointer"
               >
-                x{/* <AiFillCloseCircle className="relative right-5 " /> */}
+                x
               </div>
             </>
           );
