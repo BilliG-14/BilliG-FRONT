25c0f85893cb3866a3e0a801fe92b56ef963fb31
5thwin <hisy2059@naver.com>
2022-12-21 16:57:12 +0900

    feat category crud

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Main from 'pages/Main';
 import LoginJoin from './pages/LoginJoin';
 import AdminMain from './pages/AdminMain';
@@ -21,36 +22,43 @@ const GlobalStyle = createGlobalStyle`
   ${reset}
 `;
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <React.Fragment>
-      <GlobalStyle />
-      <div className="App h-screen w-screen">
-        <BrowserRouter>
-          {/* ScrollToTop : navigate했을 때, 스크롤 위치가 그대로 적용되는 문제 방지*/}
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/login" element={<LoginJoin />} />
-            <Route path="/admin" element={<AdminMain />} />
-            <Route path="/write/lend" element={<LendWriting />} />
-            <Route path="/write/borrow" element={<BorrowWriting />} />
-            <Route path="/submain" element={<Submain />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/read/lend" element={<LendPostDetail />} />
-            <Route path="/read/borrow" element={<BorrowPostDetail />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/mypage/givelist" element={<MyGivePostListPage />} />
-            <Route
-              path="/mypage/borrowlist"
-              element={<MyBorrowPostListPage />}
-            />
-            <Route path="/mypage/donegivelist" element={<MyDoneListPage />} />
-            <Route path="/mypage/doneborrowlist" element={<MyDoneListPage />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </React.Fragment>
+    <QueryClientProvider client={queryClient}>
+      <React.Fragment>
+        <GlobalStyle />
+        <div className="App h-screen w-screen">
+          <BrowserRouter>
+            {/* ScrollToTop : navigate했을 때, 스크롤 위치가 그대로 적용되는 문제 방지*/}
+            <ScrollToTop />
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/login" element={<LoginJoin />} />
+              <Route path="/admin" element={<AdminMain />} />
+              <Route path="/write/lend" element={<LendWriting />} />
+              <Route path="/write/borrow" element={<BorrowWriting />} />
+              <Route path="/submain" element={<Submain />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/read/lend" element={<LendPostDetail />} />
+              <Route path="/read/borrow" element={<BorrowPostDetail />} />
+              <Route path="/mypage" element={<MyPage />} />
+              <Route path="/mypage/givelist" element={<MyGivePostListPage />} />
+              <Route
+                path="/mypage/borrowlist"
+                element={<MyBorrowPostListPage />}
+              />
+              <Route path="/mypage/donegivelist" element={<MyDoneListPage />} />
+              <Route
+                path="/mypage/doneborrowlist"
+                element={<MyDoneListPage />}
+              />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </React.Fragment>
+    </QueryClientProvider>
   );
 }
 
--- client/src/components/admin/AdminCategorySection.tsx
+++ client/src/components/admin/AdminCategorySection.tsx
@@ -1,8 +1,114 @@
+import axios, { AxiosError } from 'axios';
+import { useCallback, useRef } from 'react';
+import { useQuery, useMutation, QueryClient } from 'react-query';
+
+type Category = {
+  _id: string;
+  name: string;
+};
+
+/*Category CRUD */
+const apiCategory = {
+  GET: async () => {
+    const { data } = await axios.get(
+      'https://port-0-village-dpuqy925lbn63gyo.gksl2.cloudtype.app/category',
+    );
+    return data;
+  },
+  CREATE: async (catogoryName: string) => {
+    await axios.post(
+      'https://port-0-village-dpuqy925lbn63gyo.gksl2.cloudtype.app/category',
+      JSON.stringify({ name: catogoryName }),
+      {
+        headers: { 'Content-Type': `application/json` },
+      },
+    );
+  },
+  UPDATE: async ({ _id, name }: Category) => {
+    await axios.patch(
+      `https://port-0-village-dpuqy925lbn63gyo.gksl2.cloudtype.app/category/${_id}`,
+      JSON.stringify({ name: name }),
+      {
+        headers: { 'Content-Type': `application/json` },
+      },
+    );
+  },
+  DELETE: async (_id: string) => {
+    await axios.delete(
+      `https://port-0-village-dpuqy925lbn63gyo.gksl2.cloudtype.app/category/${_id}`,
+    );
+  },
+};
+
 export default function AdminCategorySection() {
-  const datas = [
-    { _id: '001', name: '의류' },
-    { _id: '002', name: '카메라' },
-  ];
+  const queryClient = new QueryClient();
+  const selectedCategory = useRef<Category>({ _id: '', name: '' });
+  const selectedDiv = useRef<HTMLDivElement>(null);
+  const elemCreateInput = useRef<HTMLInputElement>(null);
+  const elemUpdateInput = useRef<HTMLInputElement>(null);
+  /*get category */
+  const { isLoading, data, isError } = useQuery<Category[], AxiosError>(
+    'category',
+    apiCategory.GET,
+    {
+      refetchOnWindowFocus: false, // react-query는 사용자가 사용하는 윈도우가 다른 곳을 갔다가 다시 화면으로 돌아오면 이 함수를 재실행합니다. 그 재실행 여부 옵션 입니다.
+      retry: 0, // 실패시 재호출 몇번 할지
+      onSuccess: (_data) => {
+        // 성공시 호출
+        console.log(_data);
+      },
+      onError: (e: Error) => {
+        console.log(e.message);
+      },
+    },
+  );
+  const createMutation = useMutation(apiCategory.CREATE, {
+    onSuccess: () => {
+      // post요청 성공 시 category 맵핑된 useQuery api 함수를 실행
+      return queryClient.invalidateQueries('category');
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+  const updateMutation = useMutation(apiCategory.UPDATE, {
+    onSuccess: (_data) => {
+      console.log(_data);
+      return queryClient.invalidateQueries('category');
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+  const deleteMutation = useMutation(apiCategory.DELETE, {
+    onSuccess: (_data) => {
+      return queryClient.invalidateQueries('category');
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+  /*버튼 클릭 시 실행할 함수 */
+  const handleCreate = useCallback(
+    (newCategoryName: string) => {
+      createMutation.mutate(newCategoryName);
+      alert(`${newCategoryName} 생성되었습니다.`);
+    },
+    [createMutation],
+  );
+  const handleDelete = useCallback(
+    (categoryId: string) => {
+      deleteMutation.mutate(categoryId);
+    },
+    [deleteMutation],
+  );
+  const handleUpdate = useCallback(
+    (categoryId: string, newCategoryName: string) => {
+      updateMutation.mutate({ _id: categoryId, name: newCategoryName });
+    },
+    [updateMutation],
+  );
+
   return (
     <section className="w-full text-b-text-black p-2">
       <div className="w-2/3 mx-auto mt-12">
@@ -11,33 +117,87 @@ export default function AdminCategorySection() {
             className="w-4/5 h-10 border-solid border-2 rounded-lg px-4
         focus:outline-none focus:border-4 mr-2"
             placeholder="새 카테고리 이름을 입력해주세요"
+            ref={elemCreateInput}
           />
-          <button className="w-1/6 h-10 hover:text-white border-2 text-b-text-black border-b-yellow hover:bg-b-yellow focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm">
+          <button
+            className="w-1/6 h-10 hover:text-white border-2 text-b-text-black border-b-yellow hover:bg-b-yellow focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm"
+            onClick={() => {
+              const newCategoryName = elemCreateInput.current?.value;
+              if (!newCategoryName) {
+                alert('카테고리 이름을 입력해주세요');
+                return;
+              }
+              handleCreate(newCategoryName);
+              elemCreateInput.current.value = '';
+            }}
+          >
             등록하기
           </button>
         </div>
         <div className="flex mt-8">
           <div className="w-1/2 pl-4 text-lg font-bold ">
-            <ul>
-              {datas.map((data) => {
-                return (
-                  <li key={data._id} className="hover:text-b-yellow mb-1">
-                    <a href="#!">{data.name}</a>
-                  </li>
-                );
-              })}
-            </ul>
+            {isLoading ? (
+              <p>Loading...</p>
+            ) : isError ? (
+              <p>카테고리를 불러오지 못했습니다</p>
+            ) : (
+              <ul>
+                {data?.map((category) => {
+                  return (
+                    <li key={category._id} className="hover:text-b-yellow mb-1">
+                      <a
+                        href="#!"
+                        onClick={() => {
+                          selectedCategory.current = {
+                            _id: category._id,
+                            name: category.name,
+                          };
+                          console.log(selectedCategory.current);
+                          if (selectedDiv.current) {
+                            selectedDiv.current.style.display = 'block';
+                          }
+                          if (elemUpdateInput.current) {
+                            elemUpdateInput.current.defaultValue =
+                              selectedCategory.current.name;
+                          }
+                        }}
+                      >
+                        {category.name}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
           </div>
-          <div className="w-1/2 pl-4 text-lg font-bold ">
+          <div
+            className="w-1/2 pl-4 text-lg font-bold hidden"
+            ref={selectedDiv}
+          >
             <input
               className="w-full h-10 border-solid border-2 rounded-lg px-4
         focus:outline-none focus:border-4 mt-8"
-              placeholder="의류"
+              ref={elemUpdateInput}
             />
-            <button className="w-full mt-4 h-8 rounded-lg bg-green-500 text-white transition-colors hover:bg-gradient-to-r from-lime-800 to-green-400">
+            <button
+              className="w-full mt-4 h-8 rounded-lg bg-green-500 text-white transition-colors hover:bg-gradient-to-r from-lime-800 to-green-400"
+              onClick={() => {
+                if (elemUpdateInput.current)
+                  handleUpdate(
+                    selectedCategory.current._id,
+                    elemUpdateInput.current.value,
+                  );
+              }}
+            >
               수정하기
             </button>
-            <button className="w-full mt-4 h-8 rounded-lg bg-red-400 text-white hover:bg-gradient-to-r from-rose-700 to-red-300">
+            <button
+              className="w-full mt-4 h-8 rounded-lg bg-red-400 text-white hover:bg-gradient-to-r from-rose-700 to-red-300
+            "
+              onClick={() => {
+                handleDelete(selectedCategory.current._id);
+              }}
+            >
               삭제하기
             </button>
           </div>
