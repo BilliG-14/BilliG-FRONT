6386c63d9e58a69aacd2a4211a38a4b03dbbac1c
FROM-minyoung <918liter@gmail.com>
2022-12-22 01:56:19 +0900

    Feat: 로고+메뉴버튼 구현

--- client/src/components/nav/Nav.tsx
+++ client/src/components/nav/Nav.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FiSearch } from 'react-icons/fi';
 import useLoginJoinStore from 'store/LoginJoinStore';
+import MenuButton from 'components/MenuButton/MenuButton';
 
 function Nav() {
   const [setSelectedJoin, setSelectedLogin] = useLoginJoinStore((state) => [
@@ -27,16 +28,17 @@ function Nav() {
     navigate('/search');
   };
   return (
-    <div className="flex justify-between pr-5 h-32">
-      <div>
+    <div className="flex justify-between pr-5 h-40 mt-1">
+      {/* <div className="">
         <button
           className="home  hover:text-b-yellow hover: ease-in-out duration-300"
           onClick={goHome}
         >
           <h1 className="font-extrabold text-5xl mt-9">B illi G</h1>
         </button>
-        {/* 위의 button과 h1태그를 지우고 여기에 빌리지 로고를 넣으면 됩니다! */}
-      </div>
+        {/* 위의 button과 h1태그를 지우고 여기에 빌리지 로고를 넣으면 됩니다! 
+      </div> */}
+      <MenuButton />
       <div className="flex flex-col justify-center">
         <div className="flex justify-between items-center w-52 text-lg font-semibold">
           <button
