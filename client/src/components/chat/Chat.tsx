cfd2cc2aaf239a05d17765205fa59b665c3e4be6
Gyuri092 <ggr0927@gmail.com>
2022-12-26 18:54:27 +0900

    feat: Chat.tsx 수정중

--- client/src/components/chat/Chat.tsx
+++ client/src/components/chat/Chat.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
-
+import React, { useCallback, useEffect } from 'react';
+import Content from '../Content/Content';
+import SideBar from '../SideBar/SideBar';
+import { useState } from 'react';
+import io from 'socket.io-client';
+import { useNavigate } from 'react-router-dom';
 function Chat({ chatService, username, baseURL }: any) {
-  return <div>Chat</div>;
+  return (
+    <div className="app">
+      <SideBar
+        roomList={myChatList}
+        addMyChat={addMyChat}
+        message={message}
+        currentRoom={currentRoom}
+        onClickRoom={onClickRoom}
+        onRoomListBtn={onRoomListBtn}
+        onNewChatBtn={onNewChatBtn}
+        username={username}
+      />
+      <Content
+        roomList={...activedRooms}
+        currentRoom={currentRoom}
+        onClickRoom={onClickRoom}
+        addMyChat={addMyChat}
+        user={username}
+        sendMessage={sendMessage}
+        messages={messages}
+        users={users}
+        myChatList={myChatList}
+        message={message}
+      />
+    </div>
+  );
 }
-
 export default Chat;
