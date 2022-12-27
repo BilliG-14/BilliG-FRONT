2b82e894c66abea3c1d64971ecf1a6ac418da389
Gyuri092 <ggr0927@gmail.com>
2022-12-27 19:16:40 +0900

    conflict: dev pull 이후 충돌 해결

--- client/src/components/postDetail/LendButtons.tsx
+++ client/src/components/postDetail/LendButtons.tsx
@@ -2,11 +2,15 @@ import DealDoneModal from './DealDoneModal';
 import { ProductReturnedModal } from './ProductReturnedModal';
 import { PostIdType } from './../../store/PostReadStore';
 import { ProductReceiveButton } from './ProductReceiveModal';
+import { useNavigate } from 'react-router-dom';
 
 // 게시글이 빌려주기 상태일때의 버튼들 컴포넌트
 export default function LendButtons(props: PostIdType) {
+  const navigate = useNavigate();
+
   const { postType, postId, stateNumber, loginedUserId, authorId, borrowerId } =
     props;
+
   return (
     <>
       {/* 로그인한 유저가 작성자일 때 */}
@@ -48,7 +52,12 @@ export default function LendButtons(props: PostIdType) {
           거래중
         </button>
       ) : (
-        <button className="w-1/2 h-[50px] focus:outline-none bg-b-bg-gray hover:bg-b-yellow hover:text-white font-medium rounded-lg text-sm px-5 py-2.5 transition duration-300">
+        <button
+          className="w-1/2 h-[50px] focus:outline-none bg-b-bg-gray hover:bg-b-yellow hover:text-white font-medium rounded-lg text-sm px-5 py-2.5 transition duration-300"
+          onClick={() => {
+            navigate('/chat');
+          }}
+        >
           채팅하기
         </button>
       )}
