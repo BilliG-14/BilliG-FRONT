f52429c470ea2c4df619d5f320b4ccfbae462303
FROM-minyoung <918liter@gmail.com>
2022-12-27 17:25:39 +0900

    Fix: BilliG 버튼 누르면 홈으로 이동 링크 추가

--- client/src/components/MenuButton/MenuButton.tsx
+++ client/src/components/MenuButton/MenuButton.tsx
@@ -1,6 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom';
 import './MenuButton.css';
-import billig from '../../../public/img/billig_black.svg';
 
 const MenuButton = () => {
   const navigate = useNavigate();
