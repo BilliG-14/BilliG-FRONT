30642cb11ac937c4ceb64cd10fedc306ac733ee2
Gyuri092 <ggr0927@gmail.com>
2022-12-28 05:37:32 +0900

    feat: userpanel 수정

--- client/src/components/chat/SidePanel/UserPanel.tsx
+++ client/src/components/chat/SidePanel/UserPanel.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { IoIosChatboxes } from 'react-icons/io';
-import getUserInfo from '../getUserInfo';
 import { useNavigate } from 'react-router-dom';
 
 /** 추후 any 바꾸기! */
 function UserPanel({ user }: any) {
   const navigate = useNavigate();
-  console.log('user', user.image);
   return (
     <div className="w-full h-full px-2">
       {/* Logo */}
@@ -18,7 +16,7 @@ function UserPanel({ user }: any) {
 
       <div className="flex mt-6 flex-col w-full px-6">
         <img
-          // src={user && user.photoURL} // url은 추후 다시 ...
+          // src={user && user.image} // url은 추후 다시 ...
           src={user && user.image}
           className="w-32 h-32 rounded-full object-cover border border-solid border-gray-300 bg-white"
           alt="사용자이미지"
