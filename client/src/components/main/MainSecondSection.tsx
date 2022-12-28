fad241f750da7be30c0193fdd4c1ab9039590f7b
5thwin <hisy2059@naver.com>
2022-12-29 03:11:31 +0900

    feat top button shadow 추가

--- client/src/components/main/MainSecondSection.tsx
+++ client/src/components/main/MainSecondSection.tsx
@@ -74,10 +74,6 @@ export default function MainSecondSection() {
           <WrapList />
         </div>
       </div>
-      {/* <SpeechBubbleLeft content="너무 비싸서 사는 건 부담이에요" />
-        <SpeechBubbleRight content="안쓰는 걸 빌려주고 돈을 벌어요!" />
-        <SpeechBubbleLeft content="환경오염을 줄이고 싶어요" />
-        <SpeechBubbleRight content="필요한 사람에게 대여해줄래요!" /> */}
     </section>
   );
 }
