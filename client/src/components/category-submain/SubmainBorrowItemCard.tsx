68c38d21514af4a62f8ad52f297e90c48acbfaee
Daekuen <daekuenhan@gmail.com>
2023-01-06 00:01:35 +0900

    fix : 불필요 code 수정

--- client/src/components/category-submain/SubmainBorrowItemCard.tsx
+++ client/src/components/category-submain/SubmainBorrowItemCard.tsx
@@ -52,12 +52,6 @@ export default function SubmainBorrowItemCard({
           {tradeWay.delivery ? <DealTag deal="택배거래" /> : null}
         </div>
         <div className="price text-right">
-          {/* <p className="per_time mb-2">
-            <span className="font-semibold">
-              {`${price.priceTime.toLocaleString('ko-KR')}원`}
-            </span>
-            <span className="text-xs"> / 시간</span>
-          </p> */}
           <p className="per_day">
             <span className="font-semibold">{`${price.priceDay.toLocaleString(
               'ko-KR',
