eb57857f3eccfd63b013accdc57020ca3b77acc2
5thwin <hisy2059@naver.com>
2023-01-09 19:17:27 +0900

    git System darkmode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -71,18 +71,6 @@ function App() {
 
     getUserInfo();
   }, [isLogin]);
-  /*dark mode */
-  useEffect(() => {
-    if (
-      localStorage.theme === 'dark' ||
-      (!('theme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
-    ) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  }, []);
 
   return (
     <React.Fragment>
