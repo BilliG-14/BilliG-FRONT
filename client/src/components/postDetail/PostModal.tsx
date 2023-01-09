c03eaf2860491ea331d69eaa615134601eedfa6a
FROM-minyoung <918liter@gmail.com>
2023-01-09 22:34:14 +0900

    refactoring: 거래 프로세스 버튼 css 수정

--- client/src/components/postDetail/PostModal.tsx
+++ client/src/components/postDetail/PostModal.tsx
@@ -62,7 +62,11 @@ export default function PostModal({
                 창 닫기
               </button>
               <button
-                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold text-sm px-6 py-2 rounded shadow hover:bg-emerald-700 outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                className={`${
+                  type === 'receive'
+                    ? 'bg-emerald-500 active:bg-emerald-600 hover:bg-emerald-700'
+                    : 'bg-rose-600 active:bg-rose-700 hover:bg-rose-800'
+                } text-white font-bold text-sm px-6 py-2 rounded shadow outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150`}
                 type="button"
                 onClick={changeState}
               >
